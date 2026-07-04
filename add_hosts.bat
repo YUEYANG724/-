e8a810816a55251851de823385fb83501ae5fe56
YUEYANG724 <2847130575@qq.com>
2026-07-04 18:22:50 +0800

    s

--- add_hosts.bat
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-echo 正在添加域名映射...
-echo 127.0.0.1 youzizhijia.local >> %SystemRoot%\System32\drivers\etc\hosts
-echo 完成！现在可以用 http://youzizhijia.local:5000/ 访问了
-pause
--- add_hosts.ps1
+++
@@ -1,4 +0,0 @@
-﻿Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "
-# youzizhijia.local
-127.0.0.1 youzizhijia.local" -Encoding UTF8
-Write-Host "done"
